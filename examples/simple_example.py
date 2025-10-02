b429b309d285a29aa2bfae9d35e5e6b13d989040
Lucas Michelin <lucasazevedo.michelin@gmail.com>
2025-10-02 13:12:00 -0300

    modifying simple example

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,6 +1,7 @@
 import torch
 import geospaNN
 import numpy as np
+import ipdb
 
 # 1. Define the Friedman's function, and specify the dimension of input covariates.
 def f5(X): return (10*np.sin(np.pi*X[:,0]*X[:,1]) + 20*(X[:,2]-0.5)**2 + 10*X[:,3] +5*X[:,4])/6
@@ -10,22 +11,35 @@ p = 5; funXY = f5
 # 2. Set the parameters for the spatial process.
 sigma = 1
 phi = 3/np.sqrt(2)
-rho = 0.8
 tau = 0.01
+rho = 0.8
 theta = torch.tensor([sigma, phi, tau, rho])
 # theta2 
 
 # 3. Set the hyperparameters of the data.
 #n = 1000            # Size of the simulated sample.
-n = 10            # Size of the simulated sample.
-nn = 5             # Neighbor size used for NNGP.
+n = 100            # Size of the simulated sample.
+nn = 10             # Neighbor size used for NNGP.
 batch_size = 50     # Batch size for training the neural networks.
 
 # Simulate and split the data
 # 1. Simulate the spatially correlated data with spatial coordinates randomly sampled on a [0, 10]^2 squared domain.
 torch.manual_seed(2024)
 
-X, Y, coord, cov, corerr = geospaNN.Simulation(n, p, nn, funXY, theta, range=[0, 10], time_range = 10, spatio_temporal = True)
+#X, Y, coord, cov, corerr = geospaNN.Simulation(n, p, nn, funXY, theta, range=[0, 10], time_range = 10, spatio_temporal = True)
+
+# Full GP (without nugget, for now)
+X, Y, coord, cov, corerr = geospaNN.Simulation(
+    n=n, 
+    p=p, 
+    neighbor_size=nn, 
+    fx= funXY, 
+    theta = theta[:3], 
+    range=[0, 10], 
+    time_range = 10, 
+    spatio_temporal = False,
+    isNNGP = True
+)
 
 # 2. Order the spatial locations by max-min ordering.
 X, Y, coord, _ = geospaNN.spatial_order(X, Y, coord, method = 'max-min')
@@ -34,7 +48,7 @@ X, Y, coord, _ = geospaNN.spatial_order(X, Y, coord, method = 'max-min')
 data = geospaNN.make_graph(X, Y, coord, nn)
 
 # 4. Split data into training, validation, testing sets.
-data_train, data_val, data_test = geospaNN.split_data(X, Y, coord, neighbor_size=20, test_proportion=0.2)
+data_train, data_val, data_test = geospaNN.split_data(X, Y, coord, neighbor_size=nn, test_proportion=0.2)
 
 # Compose the mlp structure and train
 # 1. Define the mlp structure (torch.nn) to use.            
@@ -57,8 +71,7 @@ model = geospaNN.nngls(p=p, neighbor_size=nn, coord_dimensions=2, mlp=mlp, theta
 nngls_model = geospaNN.nngls_train(model, lr =  0.01, min_delta = 0.001)
 
 # 4.Train the model.
-training_log = nngls_model.train(data_train, data_val, data_test,
-                                 Update_init = 10, Update_step = 10)
+training_log = nngls_model.train(data_train, data_val, data_test, Update_init = 10, Update_step = 10)
 
 train_estimate = model.estimate(data_train.x)
 test_predict = model.predict(data_train, data_test)
